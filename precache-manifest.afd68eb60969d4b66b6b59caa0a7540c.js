7a672fe1f32ae4043c5466225469cae9e59e733e
haileyli <hailey@Haileys-MacBook-Air-2.local>
2020-04-07 23:36:25 -0400

    Updates

---
+++ precache-manifest.afd68eb60969d4b66b6b59caa0a7540c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9323e7e8aa13ec520e9cbb5cbc282212",
+    "url": "/MangodbStitchTest/index.html"
+  },
+  {
+    "revision": "0fb1d2ec83378d2ad700",
+    "url": "/MangodbStitchTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33a6c6d0c2afa674645f",
+    "url": "/MangodbStitchTest/static/js/2.29402bb8.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/MangodbStitchTest/static/js/2.29402bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb1d2ec83378d2ad700",
+    "url": "/MangodbStitchTest/static/js/main.5d2ad7e1.chunk.js"
+  },
+  {
+    "revision": "0b5eb47be4910632f52d",
+    "url": "/MangodbStitchTest/static/js/runtime-main.2a97aaf3.js"
+  }
+]);
